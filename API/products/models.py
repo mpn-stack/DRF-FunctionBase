b8ce9180496bc7058b8eb77c6f405de8cafcbd2c
MPN <mpninfomail@gmail.com>
2024-02-03 19:48:00 +0330

    fix Dockerfile problems and change django version on requirements.txt

--- API/products/models.py
+++ API/products/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from datetime import datetime
+from django.utils import timezone 
 
 class Product(models.Model):
     name=models.CharField(max_length=50)
     count=models.IntegerField(default=0)
     company_name=models.CharField(max_length=50)
-    register_date=models.DateTimeField(default=datetime.now())
+    register_date=models.DateTimeField(default=timezone.now)
